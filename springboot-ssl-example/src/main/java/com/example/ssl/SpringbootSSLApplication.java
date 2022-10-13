03ae7a34b5afb77ade564679299c61f54589f436
fangxingbang <fangxingbang@syriusrobotics.com>
2022-10-13 16:52:29 +0800

    feat: fix package

--- springboot-ssl-example/src/main/java/com/example/ssl/SpringbootSSLApplication.java
+++ springboot-ssl-example/src/main/java/com/example/ssl/SpringbootSSLApplication.java
@@ -1,4 +1,4 @@
-package com.example.ssh;
+package com.example.ssl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- springboot-ssl-example/src/test/java/com/example/ssl/SpringbootSshApplicationTests.java
+++ springboot-ssl-example/src/test/java/com/example/ssl/SpringbootSshApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.ssh;
+package com.example.ssl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
