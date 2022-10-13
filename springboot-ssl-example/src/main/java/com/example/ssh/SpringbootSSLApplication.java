569a8a4b0961dc909b4c4a03ddda47a03464487a
fangxingbang <fangxingbang@syriusrobotics.com>
2022-10-13 16:19:21 +0800

    feat: init springboot-ssl

---
+++ springboot-ssl-example/src/main/java/com/example/ssh/SpringbootSSLApplication.java
@@ -0,0 +1,21 @@
+package com.example.ssh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootSSLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSSLApplication.class, args);
+	}
+
+	@RequestMapping("/ssl")
+	public String helloSSL() {
+		return "hello ssl";
+	}
+}
